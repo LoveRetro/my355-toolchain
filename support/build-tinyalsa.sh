4252fdd2fa7453f7900aec1b43215fea89b17f9e
frysee <frysee@googlemail.com>
2026-02-10 23:37:12 +0100

    Chore: Missing cross file for tinyalsa

--- support/build-tinyalsa.sh
+++ support/build-tinyalsa.sh
@@ -2,9 +2,25 @@
 
 set -euo pipefail
 
+# Create a cross file for meson
+cat > /tmp/cross_file.txt <<EOF
+[binaries]
+c = '${CROSS_TRIPLE}-gcc'
+cpp = '${CROSS_TRIPLE}-g++'
+ar = '${CROSS_TRIPLE}-ar'
+strip = '${CROSS_TRIPLE}-strip'
+pkgconfig = 'pkg-config'
+
+[host_machine]
+system = 'linux'
+cpu_family = 'aarch64'
+cpu = 'aarch64'
+endian = 'little'
+EOF
+
 # tinyalsa
 git clone --depth=1 --branch v2.0.0 https://github.com/tinyalsa/tinyalsa.git /tmp/tinyalsa && \
     cd /tmp/tinyalsa && \
-    meson setup build --prefix=$SYSROOT/usr --libdir=lib -Dbuildtype=release && \
+    meson setup build --cross-file /tmp/cross_file.txt --prefix=$SYSROOT/usr --libdir=lib -Dbuildtype=release && \
     ninja -C build install && \
     cd /tmp && rm -rf /tmp/tinyalsa
